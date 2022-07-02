577ee8043eb04e151fb9c4f98073c44131dbd03f
Fazlyev Marsel <fazlyev@mail.ru>
2022-07-03 00:53:12 +0300

    bag fixed

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,25 @@
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("СТАРАЯ РЕАЛИЗАЦИЯ КАЛЬКУЛЯТОРА");
+        System.out.println("*********************************************");
+        Calculator calculator = new Calculator();
+        double oldresult = calculator.newFormula().addOperand(10).addOperand(5).calculate(Calculator.Operation.MULT).result();
+        System.out.println(oldresult);
+        System.out.println();
+        System.out.println("НОВАЯ РЕАЛИЗАЦИЯ КАЛЬКУЛЯТОРА");
+        System.out.println("*********************************************");
+        Ints adapterIntsCalculator = new IntsCalculator();
+        int adapterResult =   adapterIntsCalculator.mult(10,5);
+        System.out.println(adapterResult);
+
+
+
+
+
+
+
+    }
+
+
+
 }
